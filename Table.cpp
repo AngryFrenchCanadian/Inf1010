fcfecb481aab91ee65811867c146d5d4a37d0b6e
etboun <etienne.bourgoin@polymtl.ca>
2019-02-01 12:27:00 -0500

    en tetes Table.cpp

--- Table.cpp
+++ Table.cpp
@@ -1,6 +1,9 @@
 #include "Table.h"
 using namespace std;
 
+/**
+* Ce constructeur par défaut initialise les attributs de la table aux valeurs par défaut.
+*/
 Table::Table() {
 	capacite_ = MAXCAP;
 	nbPlats_ = 0;
@@ -10,7 +13,13 @@ Table::Table() {
 	commande_ = new Plat*[MAXCAP];
 
 }
-	
+
+/**
+* Ce constructeur par paramètres initialise les attributs de la table aux valeurs correspondantes.
+*
+* @param Le numéro de la table (id).
+* @param Le nombre de places de la table.
+*/
 Table::Table(int id, int nbPlaces) {
 	capacite_ = MAXCAP;
 	nbPlats_ = 0;
@@ -22,6 +31,9 @@ Table::Table(int id, int nbPlaces) {
 			
 }
 
+/**
+* Ce destructeur détruit les commandes passées à la table.
+*/
 Table::~Table() {
 	for (unsigned i = 0; i < nbPlats_; i++) {
 		delete commande_[i];
@@ -30,18 +42,37 @@ Table::~Table() {
 	delete[] commande_;
 			
 }
+
+/**
+* Cette méthode accède au numero de la table (id).
+*
+* @return Le numero de la table.
+*/
 int Table::getId()const {
 	return id_;
 }
 
+/**
+* Cette méthode accède au nombre de places de la table.
+*
+* @return Le nombre de places de la table.
+*/
 int Table::getNbPlaces()const {
 	return nbPlaces_;
 }
 
+/**
+* Cette méthode accède au booléen qui permet de savoir si la table est occupée par des clients.
+*
+* @return Si la table est occupée par des clients.
+*/
 bool Table::estOccupee()const {
 	return occupee_;
 }
 
+/**
+* Cette méthode efface les commandes passées à la table.
+*/
 void Table::libererTable() {
 	for (int i = 0; i < MAXCAP; i++) {
 		delete commande_[i];
@@ -51,19 +82,33 @@ void Table::libererTable() {
 	commande_ = nullptr;
 }
 
+/**
+* Cette méthode permet de placer un client à une table et la rendre occupée.
+*/
 void Table::placerClient() {
 	occupee_ = true;
 }
 
+/**
+* Cette méthode permet de créer le numéro d'une table (id).
+*/
 void Table::setId(int id) {
 	id_ = id;
 }
 
+/**
+* Cette méthode permet d'ajouter un plat à la commande d'une table.
+*/
 void Table::commander(Plat* plat) {
 	commande_[nbPlats_++] = new Plat(plat->getNom(), plat->getPrix(), plat->getCout());
 
 }
 
+/**
+* Cette méthode permet de calculer le chiffre d'affaire des commandes d'une table.
+*
+* @return Le chiffre d'affaire des commandes d'une table.
+*/
 double Table::getChiffreAffaire() {
 	double chiffreAffaire = 0;
 	for (unsigned i = 0; i < nbPlats_; i++) 
@@ -72,6 +117,10 @@ double Table::getChiffreAffaire() {
 		
 }
 
+/**
+* Cette méthode permet d'afficher l'état d'une table et d'afficher les plats qui ont été
+* commandés pour celle-ci.
+*/
 void Table::afficher()const {
 	cout << "	La table numero " << id_ << " est ";
 
