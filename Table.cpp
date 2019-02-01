e3a8addd202805fb088659c0fab7bfd71879a965
etboun <etienne.bourgoin@polymtl.ca>
2019-02-01 11:27:38 -0500

    debut en tetes menu.cpp, modif plat.cpp et table.cpp

--- Table.cpp
+++ Table.cpp
@@ -1,94 +1,94 @@
 #include "Table.h"
 using namespace std;
 
-	Table::Table() {
-		capacite_ = MAXCAP;
-		nbPlats_ = 0;
-		id_ = -1;
-		nbPlaces_ = 1;
-		occupee_ = false;
-		commande_ = new Plat*[MAXCAP];
+Table::Table() {
+	capacite_ = MAXCAP;
+	nbPlats_ = 0;
+	id_ = -1;
+	nbPlaces_ = 1;
+	occupee_ = false;
+	commande_ = new Plat*[MAXCAP];
 
-	}
+}
 	
-	Table::Table(int id, int nbPlaces) {
-		capacite_ = MAXCAP;
-		nbPlats_ = 0;
-		id_ = id;
-		nbPlaces_ = nbPlaces;
-		occupee_ = false;
-		commande_ = new Plat*[MAXCAP];
+Table::Table(int id, int nbPlaces) {
+	capacite_ = MAXCAP;
+	nbPlats_ = 0;
+	id_ = id;
+	nbPlaces_ = nbPlaces;
+	occupee_ = false;
+	commande_ = new Plat*[MAXCAP];
 		
 			
-	}
+}
 
-	Table::~Table() {
-		for (unsigned i = 0; i < nbPlats_; i++) {
-			delete commande_[i];
-			commande_[i] = nullptr;
-		}
-		delete[] commande_;
+Table::~Table() {
+	for (unsigned i = 0; i < nbPlats_; i++) {
+		delete commande_[i];
+		commande_[i] = nullptr;
+	}
+	delete[] commande_;
 			
-	}
-	int Table::getId()const {
-		return id_;
-	}
+}
+int Table::getId()const {
+	return id_;
+}
 
-	int Table::getNbPlaces()const {
-		return nbPlaces_;
+int Table::getNbPlaces()const {
+	return nbPlaces_;
+}
+
+bool Table::estOccupee()const {
+	return occupee_;
+}
+
+void Table::libererTable() {
+	for (int i = 0; i < MAXCAP; i++) {
+		delete commande_[i];
+		commande_[i] = nullptr;
 	}
+	delete[] commande_;
+	commande_ = nullptr;
+}
 
-	bool Table::estOccupee()const {
-		return occupee_;
-	}
+void Table::placerClient() {
+	occupee_ = true;
+}
 
-	void Table::libererTable() {
-		for (int i = 0; i < MAXCAP; i++) {
-			delete commande_[i];
-			commande_[i] = nullptr;
-		}
-		delete[] commande_;
-		commande_ = nullptr;
-	}
+void Table::setId(int id) {
+	id_ = id;
+}
 
-	void Table::placerClient() {
-		occupee_ = true;
-	}
+void Table::commander(Plat* plat) {
+	commande_[nbPlats_++] = new Plat(plat->getNom(), plat->getPrix(), plat->getCout());
 
-	void Table::setId(int id) {
-		id_ = id;
-	}
+}
 
-	void Table::commander(Plat* plat) {
-		commande_[nbPlats_++] = new Plat(plat->getNom(), plat->getPrix(), plat->getCout());
-
-	}
-
-	double Table::getChiffreAffaire() {
-		double chiffreAffaire = 0;
-		for (unsigned i = 0; i < nbPlats_; i++) 
-			chiffreAffaire += (commande_[i]->getPrix() - commande_[i]->getCout());
-		return chiffreAffaire;
+double Table::getChiffreAffaire() {
+	double chiffreAffaire = 0;
+	for (unsigned i = 0; i < nbPlats_; i++) 
+		chiffreAffaire += (commande_[i]->getPrix() - commande_[i]->getCout());
+	return chiffreAffaire;
 		
-	}
+}
 
-	void Table::afficher()const {
-		cout << "	La table numero " << id_ << " est ";
+void Table::afficher()const {
+	cout << "	La table numero " << id_ << " est ";
 
-		if (occupee_ == true) {
-			cout << "occupee.";
-			if (nbPlats_ != 0) {
-				cout << " Voici la commande passee par les clients. \n";
-				for (unsigned i = 0; i < nbPlats_; i++) {
-					commande_[i]->afficher();
-				}
+	if (occupee_ == true) {
+		cout << "occupee.";
+		if (nbPlats_ != 0) {
+			cout << " Voici la commande passee par les clients. \n";
+			for (unsigned i = 0; i < nbPlats_; i++) {
+				commande_[i]->afficher();
 			}
-			else
-				cout << " Mais ils n'ont rien commande pour l'instant. \n";
 		}
-
 		else
-			cout << "libre.\n";
-		//pour le formating
-		cout << "\n";
+			cout << " Mais ils n'ont rien commande pour l'instant. \n";
 	}
+
+	else
+		cout << "libre.\n";
+	//pour le formating
+	cout << "\n";
+}
--- plat.cpp
+++ plat.cpp
@@ -1,70 +1,70 @@
 #include "Plat.h"
 using namespace std;
 	
-	/**
-	* Ce constructeur par défaut initialise les attributs aux valeurs par défaut.
-	*/
-	Plat::Plat() { 
-		nom_ = "inconnu";
-		prix_ = 0;
-		cout_ = 0;
-	} 
+/**
+* Ce constructeur par défaut initialise les attributs d'un plat aux valeurs par défaut.
+*/
+Plat::Plat() { 
+	nom_ = "inconnu";
+	prix_ = 0;
+	cout_ = 0;
+} 
 
-	/**
-	* Ce constructeur par paramètres initialise les attributs aux valeurs correspondantes.
-	*/
-	Plat::Plat(string nom, double prix, double cout) {
-		nom_ = nom;
-		prix_ = prix;
-		cout_ = cout;
-	}
+/**
+* Ce constructeur par paramètres initialise les attributs d'un plat aux valeurs correspondantes.
+*/
+Plat::Plat(string nom, double prix, double cout) {
+	nom_ = nom;
+	prix_ = prix;
+	cout_ = cout;
+}
 
-	/**
-	* Cette méthode accède au nom du plat.
-	*
-	* @return Le nom du plat.
-	*/
-	string Plat::getNom()const {
-		return nom_;
-	}
+/**
+* Cette méthode accède au nom du plat.
+*
+* @return Le nom du plat.
+*/
+string Plat::getNom()const {
+	return nom_;
+}
 
-	/**
-	* Cette méthode accède au prix du plat pour le client.
-	*
-	* @return Le prix du plat.
-	*/
-	double Plat::getPrix()const {
-		return prix_;
-	}
+/**
+* Cette méthode accède au prix du plat pour le client.
+*
+* @return Le prix du plat.
+*/
+double Plat::getPrix()const {
+	return prix_;
+}
 
-	/**
-	* Cette méthode accède au coût du plat pour le restaurant.
-	*
-	* @return Le coût du plat.
-	*/
-	double Plat::getCout()const {
-		return cout_;
-	}
+/**
+* Cette méthode accède au coût du plat pour le restaurant.
+*
+* @return Le coût du plat.
+*/
+double Plat::getCout()const {
+	return cout_;
+}
 
-	/**
-	* Cette méthode permet de modifier le nom du plat.
-	*/
-	void Plat::setNom(string nom) {
-		nom_ = nom;
-	}
+/**
+* Cette méthode permet de modifier le nom du plat.
+*/
+void Plat::setNom(string nom) {
+	nom_ = nom;
+}
 
-	/**
-	* Cette méthode permet de modifier le prix du plat pour le client.
-	*/
-	void Plat::setPrix(double prix) {
-		prix_ = prix;
-	}
+/**
+* Cette méthode permet de modifier le prix du plat pour le client.
+*/
+void Plat::setPrix(double prix) {
+	prix_ = prix;
+}
 
-	/**
-	* Cette méthode permet d'afficher le nom du plat, son prix pour le client
-	* et le coût pour le restaurant. 
-	*/
-	void Plat::afficher()const {
-		cout << "	" << nom_ << " - " << prix_ << " $  ("
-			<< cout_ << " $ pour le restaurant) \n";
-	}
+/**
+* Cette méthode permet d'afficher le nom du plat, son prix pour le client
+* et le coût pour le restaurant. 
+*/
+void Plat::afficher()const {
+	cout << "	" << nom_ << " - " << prix_ << " $  ("
+		<< cout_ << " $ pour le restaurant) \n";
+}
