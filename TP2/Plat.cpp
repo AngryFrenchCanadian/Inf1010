bab6f72c56d86b345629a263521124a8dbc403fd
etboun <etienne.bourgoin@polymtl.ca>
2019-02-09 10:46:00 -0500

    Debut des en tetes

--- TP2/Plat.cpp
+++ TP2/Plat.cpp
@@ -2,51 +2,94 @@
 * Titre : Plat.cpp - Travail Pratique #2
 * Date : 18 Janvier 2019
 * Auteur : Allan BEDDOUK
+* Modifié par : Étienne Bourgoin #1955553 et Manuel Pellerin #1960929 
+* Date : 08 février 2019
 */
 
 #include "Plat.h"
 
-//constructeurs
+/**
+* Ce constructeur par défaut initialise les attributs d'un plat aux valeurs par défaut.
+*/
 Plat::Plat() {
 	nom_ = "Inconnu";
 	prix_ = 0;
 	cout_ = 0;
 }
 
+/**
+* Ce constructeur par paramètres initialise les attributs d'un plat aux valeurs correspondantes.
+*/
 Plat::Plat(string nom, double prix, double cout) {
 	nom_ = nom;
 	prix_ = prix;
 	cout_ = cout;
 }
-//getters
 
+/**
+* Cette méthode accède au nom du plat.
+*
+* @return Le nom du plat.
+*/
 string Plat::getNom() const {
 	return nom_;
 }
 
+/**
+* Cette méthode accède au prix du plat pour le client.
+*
+* @return Le prix du plat.
+*/
 double Plat::getPrix() const {
 	return prix_;
 }
 
+/**
+* Cette méthode accède au coût du plat pour le restaurant.
+*
+* @return Le coût du plat.
+*/
 double Plat::getCout() const {
 	return cout_;
 }
-//setters
+
+/**
+* Cette méthode permet de modifier le nom du plat.
+*/
 void Plat::setNom(string nom) {
 	nom_ = nom;
 }
 
+/**
+* Cette méthode permet de modifier le prix du plat pour le client.
+*/
 void Plat::setPrix(double prix) {
 	prix_ = prix;
 }
 
 
-//methodes en plus
+/**
+* Cette méthode permet d'afficher le nom du plat, son prix pour le client
+* et le coût pour le restaurant.
+* 
+* @param Le paramètre en sortie. 
+* @param Le plat à afficher.
+*
+* @return La sortie.
+*/
 ostream& operator<<(ostream& o, const Plat& plat) {
 	o << plat.getNom() << " - " << plat.getPrix()
 		<< " $ (" << plat.getCout() << "$ pour le restaurant )";
 	return o;
 }
+
+/**
+* Cette méthode permet de comparer le prix de vente de deux plats.
+*
+* @ param Le plat du menu à comparer.
+*
+* @return Le booléen qui permet de savoir quel plat est le plus cher.
+*/
 bool Plat::operator<(const Plat& plat) {
 	if (prix_ < plat.getPrix())
 		return true;
--- TP2/Table.cpp
+++ TP2/Table.cpp
@@ -2,17 +2,27 @@
 * Titre : Table.cpp - Travail Pratique #2
 * Date : 18 Janvier 2019
 * Auteur : Allan BEDDOUK
+* Modifié par : Étienne Bourgoin #1955553 et Manuel Pellerin #1960929
+* Date : 08 février 2019
 */
 
 #include "Table.h"
 
-//constructeurs
+/**
+* Ce constructeur par défaut initialise les attributs de la table aux valeurs par défaut.
+*/
 Table::Table() {
 	id_ = -1;
 	nbPlaces_ = 1;
 	nbClientsATable_ = 0;
 }
 
+/**
+* Ce constructeur par paramètres initialise les attributs de la table aux valeurs correspondantes.
+*
+* @param Le numéro de la table (id).
+* @param Le nombre de places de la table.
+*/
 Table::Table(int id, int nbPlaces) {
 	id_ = id;
 	nbPlaces_ = nbPlaces;
@@ -21,33 +31,60 @@ Table::Table(int id, int nbPlaces) {
 
 
 
-//getters
+/**
+* Cette méthode accède au numero de la table (id).
+*
+* @return Le numero de la table.
+*/
 int Table::getId() const {
 	return id_;
 }
 
+/**
+* Cette méthode accède au nombre de places de la table.
+*
+* @return Le nombre de places de la table.
+*/
 int Table::getNbPlaces() const {
 	return nbPlaces_;
 }
 
+/**
+* Cette méthode accède au nombre de clients de la table.
+*
+* @return Le nombre de clients de la table.
+*/
 int Table::getnbClientATable() const
 {
 	return nbClientsATable_;
 }
 
+/**
+* Cette méthode accède au booléen qui permet de savoir si la table est pleine.
+*
+* @return Si la table est pleine.
+*/
 bool Table::estPleine() const {
 	return nbPlaces_==0;
 }
 
+/**
+* Cette méthode accède au booléen qui permet de savoir si la table est occupée par des clients.
+*
+* @return Si la table est occupée par des clients.
+*/
 bool Table::estOccupee() const
 {
 	return nbClientsATable_!=0;
 }
 
-//setters
+/**
+* Cette méthode permet de créer le numéro d'une table (id).
+*/
 void Table::setId(int id) {
 	id_ = id;
 }
+
 
 void Table::libererTable() {
 	nbPlaces_ += nbClientsATable_;
@@ -60,6 +97,9 @@ void Table::libererTable() {
 
 }
 
+/**
+* Cette méthode permet de placer un client à une table et la rendre occupée.
+*/
 void Table::placerClient(int nbClients) {
 	nbClientsATable_ = nbClients;
 	nbPlaces_ -= nbClients;
