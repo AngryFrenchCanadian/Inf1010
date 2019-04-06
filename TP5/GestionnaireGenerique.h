d90b36d322b06bef50a91bc34022868f56b41e23
Manuel <manuel_pellerin@hotmail.ca>
2019-04-06 15:58:21 -0400

    sauce

--- TP5/GestionnaireGenerique.h
+++ TP5/GestionnaireGenerique.h
@@ -25,3 +25,20 @@ protected:
 	C conteneur_;
 };
 
+template<typename T, typename C>
+C GestionnaireGenerique<T,C>::getConteneur() const {
+	return conteneur_;
+
+}
+
+template<typename T, typename C>
+void GestionnaireGenerique<T, C>::ajouter(T t) {
+	conteneur_.insert(t);
+
+}
+
+
+template<typename T, typename C>
+int GestionnaireGenerique<T, C>::getNombreElements() const {
+	return conteneur_.size();
+}
