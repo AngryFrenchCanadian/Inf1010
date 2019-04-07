e238a1f679309d84b528829ab942ec4994cc1f2a
Manuel <manuel_pellerin@hotmail.ca>
2019-04-07 09:15:59 -0400

    foncteurs done?

--- TP5/Foncteur.h
+++ TP5/Foncteur.h
@@ -14,12 +14,24 @@ using namespace std;
 
 class FoncteurPlatMoinsCher
 { // TODO
-
+public:
+	bool operator()(const pair<string, Plat*>& platGauche, const pair<string, Plat*>& platDroite) const {
+		return (*platGauche.second < *platDroite.second);
+	}
     
 };
 
 class FoncteurIntervalle
 {
-   // TODO
+public:
+	FoncteurIntervalle(double min, double max) :min_(min), max_(max) {};
+
+	bool operator()(Plat* plat) {
+		double prixPlat = plat->getPrix();
+		return (prixPlat >= min_ && prixPlat <= max_);
+	}
+private:
+	double min_;
+	double max_;
 };
 
